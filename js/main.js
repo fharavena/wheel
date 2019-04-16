fb1baf3906680c0162f9f02e88a6bb771c64b529
fharavena <fharavena@gmail.com>
2019-04-16 12:16:16 -0400

    se agrega switch para al cambiar de curso mover rueda y/o iconos

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 function unlock_wheel(clicked_id) {
     document.getElementById("pantalla_inicial").style.display = "none";
 
-    var img = document.getElementById("rueda_imagen");
-
-    img.classList.add("change_courses");
-
-    var curso = 0;
-
-    // alert("Hello! I am an alert box!!");
     
+    //modificar si quiere mover al cambio de curso los iconos y/o rueda
+    var icono_movimiento = true;
+    var rueda_movimiento = false;
 
-    // if(clicked_id.localeCompare("0_btn")){
-    //     alert(clicked_id);
-    // }
 
+    if(rueda_movimiento){
+        var img = document.getElementById("rueda_imagen");
+        img.classList.add("change_courses");
+        setTimeout(function () {
+            img.classList.remove("change_courses");    
+        }, 300);
+    }
+
+    if(icono_movimiento){
+        var icono = document.querySelectorAll('.icono_curso');
+        for (var i = 0; i < icono.length; i++) {
+            icono[i].classList.add('icono_curso_mover');
+        }
+    
+        setTimeout(function () {
+    
+        for (var i = 0; i < icono.length; i++) {
+            icono[i].classList.remove('icono_curso_mover');
+        }
+        }, 300);
+    }
 
     // enviar hacia afuera
-    if(clicked_id==="0_btn"){
+    if (clicked_id === "0_btn") {
         console.log("0");
-        
-    }else if(clicked_id==="1_btn"){
+
+    } else if (clicked_id === "1_btn") {
         //cursos de primero medio
         console.log("1");
         document.getElementById("curso_00").href = "#biologia1";
@@ -32,31 +46,48 @@ function unlock_wheel(clicked_id) {
         document.getElementById("curso_07").href = "#matematica1";
         document.getElementById("curso_08").href = "#orientacion1";
         document.getElementById("curso_10").href = "#quimica1";
-    }else if(clicked_id==="2_btn"){
+
+        //inicio deshabilitacion ingles
+        document.getElementById("curso_05").style.cursor = "pointer";
+        //fin deshabilitacion ingles
+
+    } else if (clicked_id === "2_btn") {
         console.log("2");
         document.getElementById("curso_00").href = "#biologia2";
         document.getElementById("curso_01").href = "#evaluaciones2";
         document.getElementById("curso_02").href = "#fisica2";
         document.getElementById("curso_03").href = "#formacion2";
         document.getElementById("curso_04").href = "#historia2";
-        document.getElementById("curso_05").style.display = "none";
+
+        //inicio deshabilitacion ingles
+
+        document.getElementById("curso_05").style.cursor = "no-drop";
+        document.getElementById("curso_05").href = "#";
+
+        //fin deshabilitacion ingles
+
         document.getElementById("curso_06").href = "#lenguaje2";
         document.getElementById("curso_07").href = "#matematica2";
         document.getElementById("curso_08").href = "#orientacion2";
         document.getElementById("curso_10").href = "#quimica2";
-    }else if(clicked_id==="3_btn"){
+    } else if (clicked_id === "3_btn") {
         console.log("3");
         document.getElementById("curso_00").href = "#biologia3";
         document.getElementById("curso_01").href = "#evaluaciones3";
         document.getElementById("curso_02").href = "#fisica3";
         document.getElementById("curso_03").href = "#formacion3";
         document.getElementById("curso_04").href = "#historia3";
-        document.getElementById("curso_05").href = "#ingles3";
+        //inicio deshabilitacion ingles
+
+        document.getElementById("curso_05").style.cursor = "no-drop";
+        document.getElementById("curso_05").href = "#";
+
+        //fin deshabilitacion ingles
         document.getElementById("curso_06").href = "#lenguaje3";
         document.getElementById("curso_07").href = "#matematica3";
         document.getElementById("curso_08").href = "#orientacion3";
         document.getElementById("curso_10").href = "#quimica3";
-    }else if(clicked_id==="4_btn"){
+    } else if (clicked_id === "4_btn") {
         console.log("4");
         document.getElementById("curso_00").href = "#biologia4";
         document.getElementById("curso_01").href = "#evaluaciones4";
@@ -68,12 +99,9 @@ function unlock_wheel(clicked_id) {
         document.getElementById("curso_07").href = "#matematica4";
         document.getElementById("curso_08").href = "#orientacion4";
         document.getElementById("curso_10").href = "#quimica4";
-    }else if(clicked_id==="5_btn"){
+        document.getElementById("curso_05").style.cursor = "pointer";
+    } else if (clicked_id === "5_btn") {
         console.log("5");
-    }
-
-    setTimeout(function() {
-        img.classList.remove("change_courses");
-    }, 300);
+    }   
 
 }
